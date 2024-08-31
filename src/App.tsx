03bb8084d747b86229675b7f6bffec2b7c4ca6c9
ahua352 <ahua352@aucklanduni.ac.nz>
2024-08-31 23:51:05 +1200

    Added orbit controls

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,45 @@
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect } from "react";
 import "./App.css";
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   useEffect(() => {
     console.log("Inside initial useEffect");
 
-    let container = document.getElementById("container");
+    const container = document.getElementById("container");
     if (container) {
-      let scene = new THREE.Scene();
-      let camera = new THREE.PerspectiveCamera(
+      const scene = new THREE.Scene();
+      const camera = new THREE.PerspectiveCamera(
         45,
         container.offsetWidth / container.offsetHeight,
         0.1,
         1000
       );
+      camera.position.set(4, 4, 4);
 
-      let renderer = new THREE.WebGLRenderer();
+      const renderer = new THREE.WebGLRenderer();
       renderer.setSize(container.offsetWidth, container.offsetHeight);
       container.appendChild(renderer.domElement);
 
-      let geometry = new THREE.BoxGeometry();
-      let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-      let cube = new THREE.Mesh(geometry, material);
-      scene.add(cube);
+      const controls = new OrbitControls(camera, renderer.domElement);
+      controls.enableDamping = true;
+      controls.enablePan = false;
+      controls.minDistance = 2;
+      controls.maxDistance = 10;
+      controls.update();
 
-      camera.position.z = 5;
+      const geometry = new THREE.BoxGeometry();
+      const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+      const cube = new THREE.Mesh(geometry, material);
+      scene.add(cube);
 
       function animate() {
         requestAnimationFrame(animate);
 
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
+        controls.update();
+        // cube.rotation.x += 0.01;
+        // cube.rotation.y += 0.01;
 
         renderer.render(scene, camera);
       }
