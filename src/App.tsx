1d25de1427053a8a8a17509bfc45da3c73473303
ahua352 <ahua352@aucklanduni.ac.nz>
2024-08-31 23:35:56 +1200

    Added threejs scene and removed placeholder stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import * as THREE from "three";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log("Inside initial useEffect");
+
+    let container = document.getElementById("container");
+    if (container) {
+      let scene = new THREE.Scene();
+      let camera = new THREE.PerspectiveCamera(
+        45,
+        container.offsetWidth / container.offsetHeight,
+        0.1,
+        1000
+      );
+
+      let renderer = new THREE.WebGLRenderer();
+      renderer.setSize(container.offsetWidth, container.offsetHeight);
+      container.appendChild(renderer.domElement);
+
+      let geometry = new THREE.BoxGeometry();
+      let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+      let cube = new THREE.Mesh(geometry, material);
+      scene.add(cube);
+
+      camera.position.z = 5;
+
+      function animate() {
+        requestAnimationFrame(animate);
+
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+
+        renderer.render(scene, camera);
+      }
+
+      animate();
+    }
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>SMPL Viewer</h1>
+      <div id="container" />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
